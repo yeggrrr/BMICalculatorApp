cf7fda393454bc7e60131df3eb3afff5f381002f
yeggrrr <yjkim981103@gmail.com>
2024-05-21 16:31:54 +0900

    Initial Commit

---
+++ BMICalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculatorApp
+//
+//  Created by YJ on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
